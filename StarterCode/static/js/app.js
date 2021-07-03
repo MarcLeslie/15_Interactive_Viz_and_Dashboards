12e1c5a300a9ab4227d8c7daa41f4a10cbcb9e60
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-07-03 12:36:02 -0400

    Getting closer

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -78,13 +78,14 @@ function buildCharts(sample) {
 
         //BUBBLE CHART - HAS PRETTY MUCH THE EXACT SAME DATA AS THE BAR CHART BUT, YOU KNOW, BUBBLES
         var bubbleTime = {
-            x: otu_ids.slice(0, 10).map(otu_ids => `OTU ${otu_ids}`).reverse(),
-            y: sample_values.slice(0, 10).reverse(),
+            x: otu_ids,
+            y: sample_values,
             mode: "markers",
-            text: otu_labels.slice(0, 10).reverse(), 
+            text: otu_labels, 
             marker: {
                 size: sample_values,
-                color: otu_ids
+                color: otu_ids,
+                colorscale: "Earth"
             }
         }; 
 
@@ -94,28 +95,44 @@ function buildCharts(sample) {
             title: "Bacteria Cultures Per Sample", 
             showlegend: false, 
             height: 600,
-            width: 1200
+            width: 1400,
+            hovermode: "closest", 
+            xaxis: {title: "OTU ID"},
+            margin: {
+                top: 20,
+                right: 0,
+                bottom: 20,
+                left: 0,
+            }, 
         };
-
         Plotly.plot("bubble", bubbleData, bubbleLayout); 
-
-
-
     }); //DATA ACCESS ENDS HERE   
 };  //END OF FUNCTION BUILDCHARTS
 
 
-
-
-
-
 // INITIALIZE 
 function init() {
+        var pullDownMenu = d3.select("#selDataset"); //selDataset comes from HTML ID
+        d3.json("samples.json").then(function(data) {
+
+            var names = data.names;
+            console.log(data.names);  //You could also do (data.samples) 
+
+            names.forEach((sample) => {
+                pullDownMenu.append("option").property("value" , sample).text(sample); //this creates under the ID selDataset in Insepct and gives you all the IDS; .property gives dropdown, .text gives you the IDs
+            });
+        });
     buildMetadata(940); //adding 940 here tells it to run the above function just for person # 940
     buildCharts(940); 
-
 }; 
 
+// Make it so that the change in sample ID = change in data
+function optionChanged(nextSample) {
+    buildMetadata(nextSample); //So when you change the sample ID, change the metadata, which changes the data you pull....
+    buildCharts(nextSample); //....which changes the charts 
+}
+
+// INITIALIZE ALL OF IT
 init(); 
 
 
