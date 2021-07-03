4c89581a6946ac663f42f551ebcb82875d7e4f70
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-07-03 10:24:55 -0400

    Still going

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,7 @@
 //POPULATE DROPDOWN 
 function buildMetadata(sample) {
 
-    //GET YOUR DATA
+    //GET YOUR DATA 
     d3.json("samples.json").then(function(data) {
         console.log(data);  //You could also do (data.samples) 
         var metadata = data.metadata; 
@@ -21,17 +21,54 @@ function buildMetadata(sample) {
         panel.html(""); 
 
         //obj entries allows you to access key/value pair data - iterate over them
-        Object.entries(result).forEach(([key, value]) => {
-            panel.append("h5"); //this creates a blank h5 tag for each key/value pair
-        })
-
+        Object.entries(result).forEach(([key, value]) => {   //pull from the var result the info 
+            panel.append("h5").text(`${key.toUpperCase()}:${value}`); //this creates a blank h5 tag for each key/value pair - can see if you use inspect in live server; adding in .text key/value populates the data
+        });
     });
-}; //END OF FUNCTION  BUILD METADATA
+}; //END OF FUNCTION  BUILDMETADATA
+
+
+
+//BAR CHART
+// Values = sample_values
+// Labels = otu_ids
+// Hovertext = otu_labels 
+function buildCharts(sample) {
+    //GET YOUR DATA 
+    d3.json("samples.json").then(function(data) {
+        console.log(data);  //You could also do (data.samples) 
+        var samples = data.samples; //calls out the subset of data that has the data for this section 
+        console.log(samples); 
+
+        //FILTER DATA FOR JUST ONE PERSON 
+        // sampleObj is the same as forEachRow
+        var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+        console.log(resultArray); //This gives you the array for just one person
+
+        //pull out the data from the array 
+        var result = resultArray[0]; 
+        console.log(result); 
+
+        //parse it - now that it's been pulled out 
+        var sample_values = result.sample_values; 
+        console.log(sample_values); 
+        var otu_ids = result.otu_ids;
+        console.log(otu_ids); 
+        var otu_labels = result.otu_labels; 
+        console.log(otu_labels); 
+
+
+
+    });   
+};  //END OF FUNCTION BUILDCHARTS
+
+
 
 // INITIALIZE 
 function init() {
     buildMetadata(940); //adding 940 here tells it to run the above function just for person # 940
-}
+    buildCharts(940); 
+}; 
 
 init(); 
 
@@ -47,11 +84,7 @@ init();
 
 
 
-// PART 1: DEMO INFO
-// PART 2: BAR CHART
-// PART 3: BUBBLE CHART
-// BONUS: RPM CHART
-// WILL NEED TO START WITH INIT
+
 
 
 
@@ -65,10 +98,8 @@ init();
 // SAME CONCEPT AS THE TABLE SECTION FOR THE UFO HW
 
 
-//Display a default plot by using the init function WHICH YOU CALL AT THE END
-// Values = sample_values
-// Labels = otu_ids
-// Hovertext = otu_labels 
+
+
 
 // FROM UFO HW - THIS IS HOW YOU POPULATE THE DROPDOWN 
 // filteredData.forEach((UFO) => {
